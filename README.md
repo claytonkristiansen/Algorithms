97ed994d760905d503ea6b438848ffe573c0e455
claytonkristiansen <clayton.kristiansen@hotmail.com>
2021-05-20 15:49:28 -0700

    grid painting done

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Algorithms
+An interactive demonstration of Dikstra's Algorithm. 
+
+Click on an intersection (vertex) to activate/deactivate it as a valid path point. 
---
+++ main.js
@@ -0,0 +1,45 @@
+
+var xUnit = 20;
+var yUnit = 20;
+var width = 5;
+var height = 5;
+
+
+var graph = new Graph(width + 1, height + 1);
+
+
+canvas = /** @type {HTMLCanvasElement} */ (document.getElementById('canvas'));
+var ctx = canvas.getContext('2d');
+
+var gridPainter = new GridPainter();
+gridPainter.drawGrid(ctx, graph, xUnit, yUnit, width, height);
+
+// Mouse event handlers (probably should be in a class)
+canvas.addEventListener('click', e => {
+    // Find nearest grid intersection
+
+    //gridPainter.drawNode(ctx, e.offsetX, e.offsetY, 'blue');
+    var nearestY = gridPainter.relativeY(e.offsetY, yUnit);
+    var nearestX = gridPainter.relativeX(e.offsetX, xUnit);
+    if(graph.Activated(nearestY/yUnit, nearestX/xUnit))
+    {
+        graph.DeativateVertex(nearestY/yUnit, nearestX/xUnit);
+    }
+    else
+    {
+        graph.ActivateVertex(nearestY/yUnit, nearestX/xUnit);
+    }
+    gridPainter.drawGrid(ctx, graph, xUnit, yUnit, width, height);
+    graph.Print();
+})
+
+// canvas.addEventListener('dblclick', e => {
+//     // Find nearest grid intersection
+
+//     //gridPainter.clearNode(ctx, e.offsetX, e.offsetY, 'blue');
+//     var nearestY = gridPainter.relativeY(e.offsetY, yUnit);
+//     var nearestX = gridPainter.relativeX(e.offsetX, xUnit);
+//     graph.ActivateVertex(nearestY/yUnit, nearestX/xUnit);
+//     gridPainter.drawGrid(ctx, graph, xUnit, yUnit, width, height);
+//     graph.Print();
+// })
