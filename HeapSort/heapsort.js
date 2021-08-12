55ded6919a3ad44208710b1d4e8ea5f8ac0880aa
claytonkristiansen <clayton.kristiansen@hotmail.com>
2021-08-11 23:26:42 -0700

    functional Heap Sort

--- HeapSort/heapsort.js
+++ HeapSort/heapsort.js
@@ -1,3 +1,5 @@
+var processingFlag = false;
+
 class MPQ
 {
     constructor()
@@ -44,19 +46,17 @@ class MPQ
     {
         let lcvalue = this.elements[MPQ.lchild(index)];
         let rcvalue = this.elements[MPQ.rchild(index)];
-        while(lcvalue < this.elements[index] || rcvalue < this.elements[index] && MPQ.lchild(index) < this.elements.length)
+        while((MPQ.lchild(index) < this.elements.length && lcvalue < this.elements[index]) || (MPQ.rchild(index) < this.elements.length && rcvalue < this.elements[index]))
         {
-            if(MPQ.rchild(index) >= this.elements.length)
+            if(lcvalue > rcvalue)
             {
-                this.swap(index, MPQ.lchild(index));
-            }
-            else if(lcvalue < rcvalue)
-            {
-                this.swap(index, MPQ.lchild(index));
+                this.swap(index, MPQ.rchild(index));
+                index = MPQ.rchild(index);
             }
             else
             {
-                this.swap(index, MPQ.rchild(index));
+                this.swap(index, MPQ.lchild(index));
+                index = MPQ.lchild(index);
             }
             lcvalue = this.elements[MPQ.lchild(index)];
             rcvalue = this.elements[MPQ.rchild(index)];
@@ -83,10 +83,25 @@ class MPQ
             return val;
         }
     }
-
-    print()
+    
+    toString()
     {
-        console.log(this.elements.toString());
+        var str = "[";
+        let i = 0;
+        while(i < this.elements.length)
+        {
+            if(i >= this.elements.length - 1)
+            {
+                str += this.elements[i];
+            }
+            else
+            {
+                str += this.elements[i] + " | ";
+            }
+            i++;
+        }
+        str += "]";
+        return str;
     }
 
     empty()
@@ -100,28 +115,143 @@ class MPQ
             return false;
         }
     }
+    clear()
+    {
+        this.elements = [];
+        processingFlag = false;
+    }
 }
+
+
 
 let mpq = new MPQ();
-mpq.push(8);
-mpq.push(9);
-mpq.push(10);
-mpq.push(7);
-mpq.push(1);
-mpq.push(2);
-mpq.push(3);
 var sortedList = [];
-while(!mpq.empty())
+
+function logHTML(text)
 {
-    sortedList.push(mpq.pop());
-    console.log(sortedList.toString());
-    mpq.print();
-}
-let i = 0;
-while(i < 10)
-{
-    console.log(i);
-    i++;
+    document.getElementById("outputtext").innerHTML += text + "<br />";
 }
 
+function PrintArray(arr)
+{
+    var str = "[";
+    let i = 0;
+    while (i < arr.length)
+    {
+        if(i >= arr.length - 1)
+        {
+            str += arr[i];
+        }
+        else
+        {
+            str += arr[i] + ", ";
+        }
+        i++;
+    }
+    str += "]";
+    return str;
+}
 
+function myPrint(mpq, sortedList, time)
+{
+    var str =   "MPQ: " + mpq.toString() + "<br />" +
+                "Sorted List: " + PrintArray(sortedList) + "<br />";
+    if(time > 0)
+    {
+        str += "Total time elapsed: " + time + " ms<br />";
+    }
+    document.getElementById("outputtext").innerHTML = str;       
+}
+
+myPrint(mpq, sortedList, 0);
+
+function onclickadd(val)
+{
+    if(isNaN(val) || processingFlag)
+    {
+        document.getElementById("valinput").value = "";
+    }
+    else if(val != "")
+    {
+        mpq.push(Number(val));
+        myPrint(mpq, sortedList, 0);
+        document.getElementById("valinput").value = "";
+    }
+}
+function oninputkeypress(event, val)
+{
+    if(isNaN(val) || processingFlag)
+    {
+        document.getElementById("valinput").value = "";
+    }
+    else if(val != "")
+    {
+        if(event.keyCode == "13")
+        {
+            mpq.push(Number(val));
+            myPrint(mpq, sortedList, 0);
+            document.getElementById("valinput").value = "";
+        }
+}
+    }
+
+function onclicknext()
+{
+    if(!mpq.empty())
+    {
+        document.getElementById("valinput").disabled = "true";
+        processingFlag = true;
+        sortedList.push(mpq.pop());
+        myPrint(mpq, sortedList, 0);
+    }
+}
+
+function onclickrun()
+{
+    var t0 = performance.now();
+    var orderedList = [];
+    while(!mpq.empty())
+    {
+        orderedList.push(mpq.pop());
+    }
+    var t1 = performance.now();
+    var totalTime = (t1 - t0);
+    myPrint(mpq, orderedList, totalTime);
+}
+
+function onclickreset()
+{
+    processingFlag = false;
+    document.getElementById("valinput").removeAttribute("disabled");
+    mpq.clear();
+    sortedList = [];
+    myPrint(mpq, sortedList, 0);
+}
+
+// mpq.push("4");
+// mpq.push("23");
+// mpq.push(324);
+// mpq.push(543);
+// mpq.push(31);
+// mpq.push(3);
+// mpq.push(1);
+// mpq.push(134);
+// mpq.push(32);
+// mpq.push(12);
+// mpq.push(12);
+// mpq.push(21);
+// mpq.push(32);
+// mpq.push(34);
+// console.log(mpq.elements);
+// mpq.pop()
+// console.log(mpq.elements);
+// mpq.pop()
+// console.log(mpq.elements);
+// mpq.pop()
+// console.log(mpq.elements);
+// mpq.pop()
+// console.log(mpq.elements);
+// mpq.pop()
+// console.log(mpq.elements);
+// mpq.pop()
+// console.log(mpq.elements);
