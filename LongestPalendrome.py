8391919e40833166dd6248e34ecf9b9626ffad7c
claytonkristiansen <clayton.kristiansen@hotmail.com>
2024-09-19 01:11:51 -0500

    Solution 2

--- LongestPalendrome.py
+++ LongestPalendrome.py
@@ -1,69 +1,101 @@
-from math import ceil, floor
+numLoops = 0
+class Solution(object):
+    def growPalendrome(self, s, substrIdx, letterSet):
+        global numLoops
+        leftPalIdx = substrIdx[0]
+        rightPalIdx = substrIdx[1]
+        numLoops += 1
 
-def strMidpoint(s, favorLeft):
-    midpoint = floor(len(s) / 2)
-    if favorLeft and (len(s) % 2) == 0 and midpoint > 0:
-        midpoint -= 1
-    return midpoint
+        if leftPalIdx > 0 and rightPalIdx < (len(s) - 1):
+            if s[leftPalIdx - 1] == s[rightPalIdx + 1]:
+                substrIdx[0] -= 1
+                substrIdx[1] += 1
+                if not (s[leftPalIdx - 1] in letterSet):
+                    letterSet.append(s[leftPalIdx - 1])
+                return True
+        if leftPalIdx > 0 and len(letterSet) <= 1:
+            if s[leftPalIdx] == s[leftPalIdx - 1]:
+                substrIdx[0] -= 1
+                if not (s[leftPalIdx - 1] in letterSet):
+                    letterSet.append(s[leftPalIdx - 1])
+                return True
+        if rightPalIdx < (len(s) - 1) and len(letterSet) <= 1:
+            if (s[rightPalIdx] == s[rightPalIdx + 1]):
+                substrIdx[1] += 1
+                if not (s[rightPalIdx + 1] in letterSet):
+                    letterSet.append(s[rightPalIdx + 1])
+                return True
 
-def growPalendrome(s, substrIdx):
-    leftPalIdx = substrIdx[0]
-    rightPalIdx = substrIdx[1]
-    leftMidpoint = strMidpoint(s, True)
-    rightMidpoint = strMidpoint(s, False)
-    # If indices of palendrome substring are within bounds
-    if leftPalIdx > 0:
-        if s[leftPalIdx] == s[leftPalIdx - 1]:
-            substrIdx[0] -= 1
-            return True
-    if rightPalIdx < (len(s) - 1):
-        if (s[rightPalIdx] == s[rightPalIdx + 1]):
-            substrIdx[1] += 1
-            return True
-    if leftPalIdx > 0 and rightPalIdx < (len(s) - 1):
-        if s[leftPalIdx - 1] == s[rightPalIdx + 1]:
-            substrIdx[0] -= 1
-            substrIdx[1] += 1
-            return True      
-    
-    # for i in range(leftPalIdx - leftMidpoint, 0, -1):
-        
+        if rightPalIdx < (len(s) - 2):
+            searchIndices = []
+            for index in range(rightPalIdx, leftPalIdx - 1, -1):
+                if s[rightPalIdx + 1] == s[index]:
+                    searchIndices.append(index)
+            for index in searchIndices:
+                checkIdx = index
+                sizeDiscovered = 1
+                done = False
+                while not done:
+                    if checkIdx == leftPalIdx:
+                        substrIdx[1] = substrIdx[1] + sizeDiscovered
+                        letterSet.extend(list(s[rightPalIdx:substrIdx[1]]))
+                        return True
+                    elif s[checkIdx - 1] == s[rightPalIdx + sizeDiscovered + 1]:
+                        sizeDiscovered += 1
+                        checkIdx -= 1
+                    else:
+                        done = True
+        if leftPalIdx > 1:
+            searchIndices = []
+            for index in range(leftPalIdx, rightPalIdx + 1):
+                if s[leftPalIdx - 1] == s[index]:
+                    searchIndices.append(index)
+            for index in searchIndices:
+                checkIdx = index
+                sizeDiscovered = 1
+                done = False
+                while not done:
+                    if checkIdx == rightPalIdx:
+                        substrIdx[0] = substrIdx[0] - sizeDiscovered
+                        letterSet.extend(list(s[substrIdx[0]:leftPalIdx]))
+                        return True
+                    elif s[checkIdx + 1] == s[leftPalIdx - sizeDiscovered - 1]:
+                        sizeDiscovered += 1
+                        checkIdx += 1
+                    else:
+                        done = True
+            
 
-    return False
+        return False
 
-def longestPalindrome(s):
-    """
-    :type s: str
-    :rtype: str
-    """
-    potentialPal = [[i,i] for i in range(0, len(s))]
-    longestSubstrLen = 0
-    longestSubstr = [0,0]
-    longestSubstr2Len2 = 0
-    longestSubstr2 = [0,0]
-    currentPal = [0,0]
-    done = False
-    while not done:
-        if not growPalendrome(s, currentPal):
-            length = currentPal[1] - currentPal[0] + 1
-            if length > longestSubstrLen:
-                longestSubstrLen = length
-                longestSubstr = currentPal[:]
-            if currentPal[1] < len(s) - 1:
-                currentPal = [currentPal[1] + 1, currentPal[1] + 1]
-            else:
-                done = True
-    while len(potentialPal) != 0:
-        for substrIdx in potentialPal[:]:
-            if not growPalendrome(s, substrIdx):
-                potentialPal.remove(substrIdx)
-                length = substrIdx[1] - substrIdx[0] + 1
-                if (length > longestSubstr2Len2):
-                    longestSubstr2Len2 = length
-                    longestSubstr2 = substrIdx
-    print(s[longestSubstr[0]:longestSubstr[1] + 1])
-    print(s[longestSubstr2[0]:longestSubstr2[1] + 1])
-    return s[longestSubstr2[0]:longestSubstr2[1] + 1]
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        longestSubstrLen = 0
+        longestSubstr = [0,0]
+        currentPal = [0,0]
+        letterSet = [s[0]]
+        done = False
+        while not done:
+            if not self.growPalendrome(s, currentPal, letterSet):
+                length = currentPal[1] - currentPal[0] + 1
+                if length > longestSubstrLen:
+                    longestSubstrLen = length
+                    longestSubstr = currentPal[:]
+                if currentPal[1] < len(s) - 1:
+                    currentPal = [currentPal[1] + 1, currentPal[1] + 1]
+                    letterSet = [s[currentPal[1]]]
+                else:
+                    done = True
+        return s[longestSubstr[0]:longestSubstr[1] + 1]
 
-s = "bannannannanna"
-longestPalStr = longestPalindrome(s)
+
+
+dut = Solution()
+s = "anananana"
+longestPalStr = dut.longestPalindrome(s)
+print(longestPalStr)
+print(f"runloop: {numLoops}")
+print(f"Str len: {len(s)}")
